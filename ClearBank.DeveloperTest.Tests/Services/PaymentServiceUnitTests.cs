8c741b7d3e5d4acf02aaffd02aa8f2e675a77511
JakeLRL <JakeLRL@hotmail.com>
2025-05-17 16:59:22 +0100

    add unit tests to cover payment logic

--- ClearBank.DeveloperTest.Tests/Services/PaymentServiceUnitTests.cs
+++ ClearBank.DeveloperTest.Tests/Services/PaymentServiceUnitTests.cs
@@ -3,11 +3,14 @@ using ClearBank.DeveloperTest.Services;
 using ClearBank.DeveloperTest.Types;
 using NSubstitute;
 using NUnit.Framework;
+using System;
 
 namespace ClearBank.DeveloperTest.Tests.Services;
 [TestFixture]
 public class PaymentServiceUnitTests
 {
+	private static readonly Random _random = new();
+
 	private IDataStore _dataStore;
 
 	private PaymentService _sut;
@@ -35,6 +38,8 @@ public class PaymentServiceUnitTests
 		// Assert
 		Assert.That(result, Is.Not.Null);
 		Assert.That(result.Success, Is.False);
+
+		_dataStore.Received(0).UpdateAccount(Arg.Any<Account>());
 	}
 
 	[TestCase(PaymentScheme.Bacs, AllowedPaymentSchemes.Bacs)]
@@ -43,19 +48,9 @@ public class PaymentServiceUnitTests
 	public void GivenValidPayment_WhenAccountSupportsPayment_ThenPaymentSuccessful(PaymentScheme paymentScheme, AllowedPaymentSchemes allowedPaymentSchemes)
 	{
 		// Arrange
-		var request = new MakePaymentRequest()
-		{
-			DebtorAccountNumber = "",
-			PaymentScheme = paymentScheme,
-			Amount = 10,
+		var request = ValidPaymentRequest(paymentScheme);
 
-		};
-		var account = new Account
-		{
-			Balance = 100,
-			AllowedPaymentSchemes = allowedPaymentSchemes,
-			Status = AccountStatus.Live
-		};
+		var account = ValidAccount(allowedPaymentSchemes);
 
 		_dataStore.GetAccount(Arg.Any<string>()).Returns(account);
 
@@ -67,5 +62,90 @@ public class PaymentServiceUnitTests
 		Assert.That(result.Success, Is.True);
 
 		_dataStore.Received(1).UpdateAccount(Arg.Any<Account>());
+	}
+
+	[TestCase(PaymentScheme.Bacs, AllowedPaymentSchemes.FasterPayments)]
+	[TestCase(PaymentScheme.FasterPayments, AllowedPaymentSchemes.Chaps)]
+	[TestCase(PaymentScheme.Chaps, AllowedPaymentSchemes.Bacs)]
+	public void GivenValidPayment_WhenAccountDoesNotSupportPayment_ThenPaymentNotSuccessful(PaymentScheme paymentScheme, AllowedPaymentSchemes allowedPaymentSchemes)
+	{
+		// Arrange
+		var request = ValidPaymentRequest(paymentScheme);
+
+		var account = ValidAccount(allowedPaymentSchemes);
+
+		_dataStore.GetAccount(Arg.Any<string>()).Returns(account);
+
+		// Act
+		var result = _sut.MakePayment(request);
+
+		// Assert
+		Assert.That(result, Is.Not.Null);
+		Assert.That(result.Success, Is.False);
+
+		_dataStore.Received(0).UpdateAccount(Arg.Any<Account>());
+	}
+
+	[Test]
+	public void GivenFasterPayment_WhenAmountGreaterThanBalance_ThenPaymentNotSuccessful()
+	{
+		// Arrange
+		var request = ValidPaymentRequest(PaymentScheme.FasterPayments);
+
+		var account = ValidAccount(AllowedPaymentSchemes.FasterPayments);
+		account.Balance = _random.Next(0, 9);
+
+		_dataStore.GetAccount(Arg.Any<string>()).Returns(account);
+
+		// Act
+		var result = _sut.MakePayment(request);
+
+		// Assert
+		Assert.That(result, Is.Not.Null);
+		Assert.That(result.Success, Is.False);
+
+		_dataStore.Received(0).UpdateAccount(Arg.Any<Account>());
+	}
+
+	[TestCase(AccountStatus.InboundPaymentsOnly)]
+	[TestCase(AccountStatus.Disabled)]
+	public void GivenChapsPayment_WhenAccountStatusNotLive_ThenPaymentNotSuccessful(AccountStatus accountStatus)
+	{
+		// Arrange
+		var request = ValidPaymentRequest(PaymentScheme.Chaps);
+
+		var account = ValidAccount(AllowedPaymentSchemes.Chaps);
+		account.Status = accountStatus;
+
+		_dataStore.GetAccount(Arg.Any<string>()).Returns(account);
+
+		// Act
+		var result = _sut.MakePayment(request);
+
+		// Assert
+		Assert.That(result, Is.Not.Null);
+		Assert.That(result.Success, Is.False);
+
+		_dataStore.Received(0).UpdateAccount(Arg.Any<Account>());
+	}
+
+	private static MakePaymentRequest ValidPaymentRequest(PaymentScheme paymentScheme)
+	{
+		return new MakePaymentRequest()
+		{
+			DebtorAccountNumber = "",
+			PaymentScheme = paymentScheme,
+			Amount = _random.Next(10,20)
+		};
+	}
+
+	private static Account ValidAccount(AllowedPaymentSchemes allowedPaymentSchemes)
+	{
+		return new Account
+		{
+			Balance = _random.Next(100, 200),
+			AllowedPaymentSchemes = allowedPaymentSchemes,
+			Status = AccountStatus.Live
+		};
 	}
 }
