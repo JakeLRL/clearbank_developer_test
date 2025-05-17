758f55a1e362aea196ac60c7db9df6266b14250c
JakeLRL <JakeLRL@hotmail.com>
2025-05-17 15:49:54 +0100

    Add basic test, all that can be covered initially

---
+++ ClearBank.DeveloperTest.Tests/Services/PaymentServiceTests.cs
@@ -0,0 +1,34 @@
+﻿using ClearBank.DeveloperTest.Services;
+using ClearBank.DeveloperTest.Types;
+using NUnit.Framework;
+
+namespace ClearBank.DeveloperTest.Tests.Services;
+[TestFixture]
+public class PaymentServiceTests
+{
+	private PaymentService _sut;
+	
+	[SetUp]
+	public void SetUp()
+	{
+		_sut = new PaymentService();
+	}
+
+	[Test]
+	public void GivenMakePayment_WhenAccountIsNull_ThenIsNotSuccessful()
+	{
+		// Arrange
+		var request = new MakePaymentRequest()
+		{
+			DebtorAccountNumber = "",
+			PaymentScheme = PaymentScheme.Bacs
+		};
+
+		// Act
+		var result = _sut.MakePayment(request);
+
+		// Assert
+		Assert.That(result, Is.Not.Null);
+		Assert.That(result.Success, Is.False);
+	}
+}
